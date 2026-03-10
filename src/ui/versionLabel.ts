d3e44da4a6c50df2a61eacd7236bad6aed1b4a06
Greg Carr <greghcarr@gmail.com>
2026-03-10 03:00:33 -0400

    Increment version to 0.2.0-pre-alpha

--- src/ui/versionLabel.ts
+++ src/ui/versionLabel.ts
@@ -1,7 +1,7 @@
 import Phaser from 'phaser';
 import { WORLD_WIDTH, GAME_HEIGHT, COLORS } from '@/data/constants';
 
-export const APP_VERSION = 'v0.1.1-pre-alpha';
+export const APP_VERSION = 'v0.2.0-pre-alpha';
 
 export function addVersionLabel(scene: Phaser.Scene, color: string = COLORS.VERSION_DEFAULT): void {
   scene.add.text(WORLD_WIDTH - 27, GAME_HEIGHT - 27, APP_VERSION, {
